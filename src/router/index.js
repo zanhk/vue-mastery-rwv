f617015d2552bd025dce11bc92372145be12f77a
Giulio <zank@outlook.it>
2021-02-25 00:28:41 +0100

    create details routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '@/views/EventList.vue'
-import About from '@/views/About.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "@/views/EventList.vue";
+import EventDetail from "@/views/EventDetail.vue";
+import About from "@/views/About.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'EventList',
+    path: "/",
+    name: "EventList",
     component: EventList
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/event/:id",
+    name: "EventDetail",
+    component: EventDetail
+  },
+  {
+    path: "/about",
+    name: "About",
     component: About
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
+	<div class="events">
+		<EventCard v-for="event in events" :key="event.id" :event="event" />
+	</div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
-  name: 'EventList',
-  components: {
-    EventCard
-  },
-  data() {
-    return {
-      events: null
-    }
-  },
-  created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-}
+	name: "EventList",
+	components: {
+		EventCard,
+	},
+	data() {
+		return {
+			events: null,
+		};
+	},
+	created() {
+		EventService.getEvents()
+			.then((response) => {
+				this.events = response.data;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	},
+};
 </script>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+	.events {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
